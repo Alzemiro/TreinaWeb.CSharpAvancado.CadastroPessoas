4cc8e7cc3a78fffe349f03c4390a6ac67d625903
Iago <iagomoraest@gmail.com>
2020-05-13 11:57:23 -0300

    Adicionar arquivos de projeto.

---
+++ TreinaWeb.CadastroPessoas.Persistencia.EF/CadastroPessoasDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreinaWeb.CadastroPessoas.Dominio;
+
+namespace TreinaWeb.CadastroPessoas.Persistencia.EF
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class CadastroPessoasDbContext : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+    }
+}
---
+++ TreinaWeb.CadastroPessoas.Repositorio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TreinaWeb.CadastroPessoas.Repositorio
+{
+    public class Class1
+    {
+    }
+}
